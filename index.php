517c2f0db3f6ff86d9fec3fb2478d6545e64116e
Extime071 <96773180+Extime071@users.noreply.github.com>
2022-01-05 13:16:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title>php веб сайт</title>
+</head>
+<body>
+          <?php require "blocks/header.php" ?>
+
+    <div class="container mt-5">
+        <h3 class="mb-5">Наши статьи</h3>
+        <div class="d-flex flex-wrap">
+       
+        <?php
+           for($i = 0; $i < 5; $i++):
+        ?>
+         <div class="card-body">
+             <img src="img/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Подробнее</button>
+          </div>
+        
+      
+      <?php endfor; ?>
+    
+    </div>
+    </div>
+
+    <?php require "blocks/footer.php" ?>
+    
+</body>
+</html>
